36d00eb31cc7ba372d56f2ca7a89f35fe5df4b98
Dâmocles Gil Marçal <damoclesgil@gmail.com>
2018-01-27 11:28:53 -0200

    zs

---
+++ sup-rocambole/app/products/product.ts
@@ -0,0 +1,11 @@
+/* Defines the product entity */
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    amount: number;
+    price: number;
+    description: string;
+    imageUrl: string;
+}
+
